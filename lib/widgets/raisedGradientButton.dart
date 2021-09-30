10baf21822db84bf2e68ddfa602448a1e82d162d
H-R-S <369haris@gmail.com>
2021-09-30 23:52:18 +0500

    Raised Gradient Button Widget  NewsAPP+

---
+++ lib/widgets/raisedGradientButton.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class RaisedGradientButton extends StatelessWidget {
+  final Widget child;
+  final Gradient gradient;
+  final double width;
+  final double height;
+  final VoidCallback onPressed;
+
+  const RaisedGradientButton({
+    required this.child,
+    required this.gradient,
+    this.width = double.infinity,
+    this.height = 50.0,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 50.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        gradient: gradient,
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+            onTap: onPressed,
+            child: Center(
+              child: child,
+            )),
+      ),
+    );
+  }
+}
