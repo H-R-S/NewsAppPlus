74c6cef2bad10da20c8722f0dcaba1fc6cdb3d72
H-R-S <369haris@gmail.com>
2021-10-08 23:29:56 +0500

    FavoriteNews feature added

--- lib/api/newsApi.dart
+++ lib/api/newsApi.dart
@@ -1,5 +1,5 @@
 class NewsApi {
-  static String newsApiUrl =
+  static String topHeadLinesNewsApiUrl =
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=3bba92f4994941f2a1c4c802fb70c0c2";
   
 }
