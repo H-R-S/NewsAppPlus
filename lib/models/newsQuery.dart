9b94b0f4680560148d32fdf3edb9e1300945d963
H-R-S <369haris@gmail.com>
2021-10-08 23:30:29 +0500

    FavoriteNews Model added

--- lib/models/newsQuery.dart
+++ lib/models/newsQuery.dart
@@ -3,18 +3,21 @@ class NewsQueryModel {
   late String newsDes;
   late String newsImg;
   late String newsUrl;
+  late bool newsFav;
 
   NewsQueryModel(
       {this.newsHead = "NEWS HEADLINE",
       this.newsDes = "SOME NEWS",
       this.newsImg = "SOME URL",
-      this.newsUrl = "SOME URL"});
+      this.newsUrl = "SOME URL",
+      this.newsFav = false});
 
   factory NewsQueryModel.fromMap(Map news) {
     return NewsQueryModel(
         newsHead: news["title"],
         newsDes: news["description"],
         newsImg: news["urlToImage"],
-        newsUrl: news["url"]);
+        newsUrl: news["url"],
+        newsFav: news[false]);
   }
 }
