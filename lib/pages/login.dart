60edc0cafe9a4dbff8ca9669a98bce0077e99f34
Muhammad Haris <369haris@gmail.com>
2021-10-16 20:37:20 +0500

    Google SignIn Fixed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:news_app_plus/home.dart';
 import 'package:news_app_plus/pages/home.dart';
 import 'package:news_app_plus/pages/loginWithEmail.dart';
@@ -25,132 +26,142 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       backgroundColor: Colors.grey.shade200,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Container(
-                      height: 80,
-                      child: Image.asset("assets/logos/newsAppLogo.png")),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Column(
+      body: SafeArea(
+        child: Container(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: SafeArea(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "Welcome To",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                          fontFamily: ""),
+                    Center(
+                      child: Container(
+                          height: 80,
+                          child: Image.asset("assets/logos/newsAppLogo.png")),
                     ),
-                    Text(
-                      "NewsAPP+",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                          fontFamily: ""),
+                    SizedBox(
+                      height: 40,
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FlatButton(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome To",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              fontFamily: ""),
+                        ),
+                        Text(
+                          "NewsAPP+",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              fontFamily: ""),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.end,
+                    //   children: [
+                    //     FlatButton(
+                    //       onPressed: () {
+                    //         Navigator.pushReplacement(context,
+                    //             MaterialPageRoute(builder: (context) => Home()));
+                    //       },
+                    //       textColor: Colors.black,
+                    //       child: Text(
+                    //         "Skip",
+                    //         style: TextStyle(
+                    //             decoration: TextDecoration.underline,
+                    //             fontSize: 15,
+                    //             fontFamily: "",
+                    //             fontWeight: FontWeight.bold),
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    GradientLabelButton(
                       onPressed: () {
-                        Navigator.pushReplacement(context,
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => SignUpPage()));
+                      },
+                      icon: Icons.email_outlined,
+                      label: "Continue with Email",
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Colors.black54,
+                          Colors.black54,
+                        ],
+                      ),
+                      labelColor: Colors.white,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GradientLabelButton(
+                      onPressed: () {
+                        // await signUpWithFacebook();
+                        // await Navigator.pushReplacement(context,
+                        //     MaterialPageRoute(builder: (context) => Home()));
+                      },
+                      icon: Icons.facebook,
+                      label: "Continue with Facebook",
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xff4267B2),
+                          Color(0xff4267B2),
+                        ],
+                      ),
+                      labelColor: Colors.white,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GradientLabelButton(
+                      onPressed: () async {
+                        await signInWithGoogle();
+                        await Navigator.pushReplacement(context,
                             MaterialPageRoute(builder: (context) => Home()));
                       },
-                      textColor: Colors.black,
-                      child: Text(
-                        "Skip",
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            fontSize: 15,
-                            fontFamily: "",
-                            fontWeight: FontWeight.bold),
+                      icon: Icons.g_mobiledata,
+                      label: "Continue with Google",
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Colors.redAccent,
+                          Colors.redAccent,
+                        ],
                       ),
+                      labelColor: Colors.white,
                     ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Text(
+                        "Already have an account?",
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginWithEmailPage()));
+                        },
+                        textColor: Colors.black,
+                        child: Text("Login"),
+                      ),
+                    ]),
                   ],
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                GradientLabelButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()));
-                  },
-                  icon: Icons.email_outlined,
-                  label: "Continue with Email",
-                  gradient: LinearGradient(
-                    colors: <Color>[
-                      Colors.black54,
-                      Colors.black54,
-                    ],
-                  ),
-                  labelColor: Colors.white,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                GradientLabelButton(
-                  onPressed: () {},
-                  icon: Icons.facebook,
-                  label: "Continue with Facebook",
-                  gradient: LinearGradient(
-                    colors: <Color>[
-                      Color(0xff4267B2),
-                      Color(0xff4267B2),
-                    ],
-                  ),
-                  labelColor: Colors.white,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                GradientLabelButton(
-                  onPressed: () {
-                    signInWithGoogle();
-                  },
-                  icon: Icons.g_mobiledata,
-                  label: "Continue with Google",
-                  gradient: LinearGradient(
-                    colors: <Color>[
-                      Colors.redAccent,
-                      Colors.redAccent,
-                    ],
-                  ),
-                  labelColor: Colors.white,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Text(
-                    "Already have an account?",
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LoginWithEmailPage()));
-                    },
-                    textColor: Colors.black,
-                    child: Text("Login"),
-                  ),
-                ]),
-              ],
+              ),
             ),
           ),
         ),
