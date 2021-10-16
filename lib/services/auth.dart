3ef29ac27dda5e81069df3c2accec1fcd3c76aa2
Muhammad Haris <369haris@gmail.com>
2021-10-16 20:39:22 +0500

    Google SignIn/SignUp auth service

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,41 +2,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
+final googles = GoogleSignIn();
 
-//SIGN IN KA Function
-Future<User?> signInWithGoogle() async
-{
-  try{
-  //SIGNING IN WITH GOOGLE
-  final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount!.authentication;
-
-  //CREATING CREDENTIAL FOR FIREBASE
-  final AuthCredential credential = GoogleAuthProvider.credential(
-    idToken: googleSignInAuthentication.idToken,
-    accessToken: googleSignInAuthentication.accessToken
+Future<UserCredential?> signInWithGoogle() async {
+  final googleUser = await googles.signIn();
+  final GoogleSignInAuthentication googleAuth =
+      await googleUser!.authentication;
+  final OAuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
   );
-
-  //SIGNING IN WITH CREDENTIAL & MAKING A USER IN FIREBASE  AND GETTING USER CLASS
-  final userCredential  = await _auth.signInWithCredential(credential);
-  final User? user = userCredential.user;
-
-  //CHECKING IS ON
-  assert(!user!.isAnonymous);
-  assert(await user!.getIdToken() != null);
-
-  final User? currentUser = await _auth.currentUser;
-  assert(currentUser!.uid == user!.uid);
-  print(user);
-  return user;
-  }catch(e){
-    print(e);
-  }
+  return await _auth.signInWithCredential(credential);
 }
 
-void signOut() async
-{
-  await googleSignIn.signOut();
-  await _auth.signOut();
+void Logout() async {
+  await googles.disconnect();
+  await FirebaseAuth.instance.signOut();
 }
